cb53b4b801f16ac4bfed40f443090b4402eb9ea8
Adya Rahmat Perdana <adyarperdana@gmail.com>
2024-08-23 21:30:47 +0700

    initial commit

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+const { verify } = require("jsonwebtoken");
+const { User } = require("../models");
+const UnauthorizedError = require("../errors/UnauthorizedError");
+
+const auth = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  try {
+    const [type, token] = authorization.split(" ");
+    const decoded = verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ where: { email: decoded.email } });
+    if (!user) throw new Error("User is not registered");
+    req.user = user;
+  } catch (error) {
+    return res.status(401).json({ error: "Unauthorized" ,message: "Please Login First!" });
+  }
+
+  next();
+};
+
+module.exports = auth;
